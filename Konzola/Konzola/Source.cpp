852c50a1b1621eb5629d3324b478ca1f009da552
d2tomljenovic <tomljenovicd@gmail.com>
2016-04-14 16:43:02 +0200

    Nakon cherry-picka

--- Konzola/Konzola/Source.cpp
+++ Konzola/Konzola/Source.cpp
@@ -3,5 +3,5 @@
 using namespace std;
 
 void main(){
-	cout << "Pozdrav iz aplikacije od Davor Tomljenovic, neznam broj indeksa" << endl;
+	cout << "Pozdrav iz aplikacije od Davor Tomljenovic" << endl;
 }
